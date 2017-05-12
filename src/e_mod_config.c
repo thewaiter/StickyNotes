f67591832dc40087e54539eb3c62e5dd4e1fc02e
thewaiter <s.uram@centrum.sk>
2017-05-12 18:46:22 +0200

    area text section has to be disable when command text

--- src/e_mod_config.c
+++ src/e_mod_config.c
@@ -22,7 +22,17 @@ struct _E_Config_Dialog_Data
    {
       Evas_Object *header_label;
       Evas_Object *header_text;
+      Evas_Object *header_switcher;
+      
+      Evas_Object *area_label;
       Evas_Object *notif_entry;
+      
+      Evas_Object *area_entry1;
+      Evas_Object *area_entry2;
+      Evas_Object *area_entry3;
+      Evas_Object *area_entry4;
+      Evas_Object *area_entry5;
+      Evas_Object *html_label;
    } ui;
 };
 
@@ -34,6 +44,7 @@ static Evas_Object *_basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dia
 static int _basic_apply(E_Config_Dialog *cfd, E_Config_Dialog_Data *cfdata);
 static void  _cb_check_changed(void *data, Evas_Object *obj __UNUSED__);
 static void  _cb_check_changed_notif(void *data, Evas_Object *obj __UNUSED__);
+static void  _cb_check_changed_command(void *data, Evas_Object *obj __UNUSED__);
 
 /* External Functions */
 
@@ -117,6 +128,7 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    e_widget_framelist_content_align_set(of, 0.0, 0.0);
    
    ow = e_widget_check_add(evas, D_(" Note text in the header"), &(cfdata->header_switch));
+   cfdata->ui.header_switcher = ow;
    e_widget_framelist_object_append(of, ow);
    e_widget_on_change_hook_set(ow, _cb_check_changed, cfdata);
       
@@ -129,22 +141,28 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    e_widget_framelist_object_append (of, ow);
 
    ow = e_widget_label_add (evas, D_("Note text"));
-   e_widget_size_min_set(ow, 120, 25);
+   cfdata->ui.area_label = ow;
    e_widget_framelist_object_append(of, ow);
    
    ow = e_widget_entry_add(evas, &(cfdata->area_text), NULL, NULL, NULL);
    e_widget_framelist_object_append(of, ow);
+   cfdata->ui.area_entry1 = ow;
    ow = e_widget_entry_add(evas, &(cfdata->area_text_2), NULL, NULL, NULL);
    e_widget_framelist_object_append(of, ow);
+   cfdata->ui.area_entry2 = ow;
    ow = e_widget_entry_add(evas, &(cfdata->area_text_3), NULL, NULL, NULL);
    e_widget_framelist_object_append(of, ow);
+   cfdata->ui.area_entry3 = ow;
    ow = e_widget_entry_add(evas, &(cfdata->area_text_4), NULL, NULL, NULL);
    e_widget_framelist_object_append(of, ow);
+   cfdata->ui.area_entry4 = ow;
    ow = e_widget_entry_add(evas, &(cfdata->area_text_5), NULL, NULL, NULL);
    e_widget_framelist_object_append(of, ow);
+   cfdata->ui.area_entry5 = ow;
+
    
    ow = e_widget_label_add (evas, D_("HTML tags: <b>, <i>, <br>, <ps>, <tab>"));
-   e_widget_size_min_set(ow, 120, 25);
+   cfdata->ui.html_label = ow;
    e_widget_framelist_object_append(of, ow);
   
    e_widget_list_object_append(o, of, 1, 0, 0.5);
@@ -157,6 +175,8 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    
    ow = e_widget_entry_add(evas, &(cfdata->command), NULL, NULL, NULL);
    e_widget_framelist_object_append(of, ow);
+   e_widget_on_change_hook_set(ow, _cb_check_changed_command, cfdata);
+    
    
    ow = e_widget_label_add(evas, D_("Refresh interval"));
    e_widget_framelist_object_append(of, ow);
@@ -177,7 +197,9 @@ _basic_create(E_Config_Dialog *cfd, Evas *evas, E_Config_Dialog_Data *cfdata)
    
    e_widget_list_object_append(o, of, 1, 0, 0.5);
    _cb_check_changed(cfdata, NULL);
+   _cb_check_changed_command(cfdata, NULL);
    _cb_check_changed_notif(cfdata, NULL);
+   
     e_dialog_resizable_set(cfd->dia, EINA_FALSE);
    return o;
 }
@@ -187,13 +209,14 @@ _cb_check_changed(void *data, Evas_Object *obj __UNUSED__)
 {
 	E_Config_Dialog_Data *cfdata;
 	if (!(cfdata = data)) return;
-  if(cfdata->header_switch) {
-    e_widget_disabled_set(cfdata->ui.header_label, EINA_TRUE);
-    e_widget_disabled_set(cfdata->ui.header_text, EINA_TRUE);
-  } else {
-    e_widget_disabled_set(cfdata->ui.header_label, EINA_FALSE);
-    e_widget_disabled_set(cfdata->ui.header_text, EINA_FALSE);
-   }
+    
+    if(cfdata->header_switch) {
+      e_widget_disabled_set(cfdata->ui.header_label, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.header_text, EINA_TRUE);
+    } else {
+      e_widget_disabled_set(cfdata->ui.header_label, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.header_text, EINA_FALSE);
+    }
 }
 
 static void  
@@ -201,11 +224,40 @@ _cb_check_changed_notif(void *data, Evas_Object *obj __UNUSED__)
 {
 	E_Config_Dialog_Data *cfdata;
 	if (!(cfdata = data)) return;
-  if(cfdata->notif_switch) {
-    e_widget_disabled_set(cfdata->ui.notif_entry, EINA_FALSE);
-  } else {
-    e_widget_disabled_set(cfdata->ui.notif_entry, EINA_TRUE);
-   }
+    if(cfdata->notif_switch) 
+      e_widget_disabled_set(cfdata->ui.notif_entry, EINA_FALSE);
+    else 
+      e_widget_disabled_set(cfdata->ui.notif_entry, EINA_TRUE);
+}
+
+static void  _cb_check_changed_command(void *data, Evas_Object *obj __UNUSED__)
+{
+	E_Config_Dialog_Data *cfdata;
+	if (!(cfdata = data)) return;
+	
+	if(strlen(cfdata->command)) {
+	  if (cfdata->header_switch)
+	  e_widget_check_checked_set(cfdata->ui.header_switcher, 0);
+  	  e_widget_disabled_set(cfdata->ui.header_switcher, EINA_TRUE);
+
+	  e_widget_disabled_set(cfdata->ui.header_text, EINA_FALSE);
+	  e_widget_disabled_set(cfdata->ui.area_label, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.area_entry1, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.area_entry2, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.area_entry3, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.area_entry4, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.area_entry5, EINA_TRUE);
+      e_widget_disabled_set(cfdata->ui.html_label, EINA_TRUE);
+    } else {
+	  e_widget_disabled_set(cfdata->ui.header_switcher, EINA_FALSE);
+  	  e_widget_disabled_set(cfdata->ui.area_label, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.area_entry1, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.area_entry2, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.area_entry3, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.area_entry4, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.area_entry5, EINA_FALSE);
+      e_widget_disabled_set(cfdata->ui.html_label, EINA_FALSE);
+    }
 }
 
 static int 
